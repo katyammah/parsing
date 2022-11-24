ddb312fbabf5746d41803f549be8e462e6f84c6f
katyammah <kat.mahova96@mail.ru>
2022-11-24 11:31:07 +0300

    second commit

--- kinopoisk/kinopoisk.py
+++ kinopoisk/kinopoisk.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+
 user_id = 51389537
 url = 'http://www.kinopoisk.ru/user/%d/votes/' % (user_id)
 
@@ -7,10 +8,5 @@ headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Ge
            "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3"}
 r = requests.get(url, headers=headers, stream=True)
 
-
-
-print(r.status_code)
-
 with open('text.html', 'w', encoding='utf-8', errors='ignore') as f:
     f.write(r.text)
-
